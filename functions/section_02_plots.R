c81bfa244f5850abb86853f5bdc767f71003ec97
Phillip Hungerford <phillip.hungerford@gmail.com>
2020-08-12 17:07:00 +1000

    added 'docstrings' to functions for clarity and removed unnecessary functions

--- functions/section_02_plots.R
+++ functions/section_02_plots.R
@@ -5,7 +5,18 @@
 ##############################################################################
 # PLOT 1: Density plot
 density_plot <- function(df, variable){
-
+  # ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+  # PURPOSE:
+  #     Create's a density plot for continuous variables
+  #
+  # INPUTS:
+  #     df: dataframe with variables
+  #     variable: variable of which to calculate density plot
+  #
+  # Returns: 
+  #     p: plot 
+  #
+  # ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
   # create label
   y_label <- paste0("Density plot for ", variable)
   
@@ -26,7 +37,18 @@ density_plot <- function(df, variable){
 #=============================================================================
 # PLOT 2: Donut
 donut_plot <- function(df, variable){
-  
+  # ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+  # PURPOSE:
+  #     Create's a donut plot for categorical variables
+  #
+  # INPUTS:
+  #     df: dataframe with variables
+  #     variable: variable of which to calculate density plot
+  #
+  # Returns: 
+  #     p: plot 
+  #
+  # ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
   # create label
   y_label <- paste0("Donut plot for ", variable,".")
   
@@ -57,6 +79,18 @@ donut_plot <- function(df, variable){
 #=============================================================================
 # PLOT 3: Histogram
 histogram_plot <- function(df, variable){
+  # ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+  # PURPOSE:
+  #     Create's a histogram plot for continuous variables
+  #
+  # INPUTS:
+  #     df: dataframe with variables
+  #     variable: variable of which to calculate density plot
+  #
+  # Returns: 
+  #     p: plot 
+  #
+  # ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
   # create label
   y_label <- paste0("Histogram of ", variable)
   
@@ -75,7 +109,18 @@ histogram_plot <- function(df, variable){
 #=============================================================================
 # PLOT 4: Box plot
 donut_summary <- function(df, variable){
-  
+  # ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+  # PURPOSE:
+  #     Create's summary data which is used for the donut plot
+  #
+  # INPUTS:
+  #     df: dataframe with variables
+  #     variable: variable of which to calculate density plot
+  #
+  # Returns: 
+  #     tmp2: dataframe/table of summarised results 
+  #
+  # ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
   # create label
   y_label <- paste0("Donut plot for ", variable,".")
   
@@ -93,93 +138,7 @@ donut_summary <- function(df, variable){
   
   return(tmp2)
 }
-##############################################################################
-##############################################################################
-##############################################################################
-proportion_plot <- function(df, variable){
-  ##############################################################################
-  t0 <- percent_ci(df, variable, variable, outcome=1, time_ind=0)
-  t1 <- percent_ci(df, variable, variable, outcome=1, time_ind=1)
-  t2 <- percent_ci(df, variable, variable, outcome=1, time_ind=2)
-  t3 <- percent_ci(df, variable, variable, outcome=1, time_ind=3)
-  t4 <- percent_ci(df, variable, variable, outcome=1, time_ind=4)
-  t5 <- percent_ci(df, variable, variable, outcome=1, time_ind=5)
-  
-  tall <- as.data.frame(rbind(t0,t1,t2,t3,t4,t5))
-  names(tall)[names(tall) == "V1"] <- 'estimate'
-  names(tall)[names(tall) == "V2"] <- 'lower'
-  names(tall)[names(tall) == "V3"] <- 'upper'
-  names(tall)[names(tall) == "V4"] <- 'time'
-  ##############################################################################
-  y_label <- paste0("Proportion of ", variable," Users")
-  p <- ggplot(data=tall, aes(x=time, y=estimate, ymin=lower, ymax=upper, group=1))+
-    #geom_line(size = 8, colour="black",) +
-    #geom_df(size=1, color='blue') +
-    geom_ribbon(alpha=0.43, fill='blue') +
-    geom_line(size=2, color='blue') +
-    labs(title="",
-         x = "Time",
-         y = y_label) + theme_bw()
-return(p)
-}
 
-
-ome_plot <- function(df, variable){
-  require(dplyr)
-  tmp <- df %>% select(time, totalopioiddose, all_of(variable))
-  names(tmp)[names(tmp) == variable] <- 'drug'
-  
-  # subset for only those drug users 
-  tmp2 <- subset(tmp, drug == 1)
-  tmp2 <- tmp2[complete.cases(tmp2),]
-  
-  # calculate the mean ome and plot it 
-  t0 <- mean(tmp2$totalopioiddose[which(tmp2$time == 0)], na.rm=T)
-  t1 <- mean(tmp2$totalopioiddose[which(tmp2$time == 1)], na.rm=T)
-  t2 <- mean(tmp2$totalopioiddose[which(tmp2$time == 2)], na.rm=T)
-  t3 <- mean(tmp2$totalopioiddose[which(tmp2$time == 3)], na.rm=T)
-  t4 <- mean(tmp2$totalopioiddose[which(tmp2$time == 4)], na.rm=T)
-  t5 <- mean(tmp2$totalopioiddose[which(tmp2$time == 5)], na.rm=T)
-  
-  time <- c(0:5)
-  means <- c(t0,t1,t2,t3,t4,t5)
-  
-  results <- data.frame(Time = time, OME = means)
-  
-  # subset by drug
-  ##############################################################################
-  y_label <- paste0("Mean oral morphine equivalent for \n", variable," users across time")
-  p <- ggplot(data=results, aes(x=Time, y=OME))+
-    geom_line(size=2, color='red') +
-    labs(title="",
-         x = "Time",
-         y = y_label) + theme_bw()
-  return(p)
-}
-
-ome_summary <- function(df, variable){
-  require(dplyr)
-  tmp <- df %>% select(time, totalopioiddose, all_of(variable))
-  names(tmp)[names(tmp) == variable] <- 'drug'
-  
-  # subset for only those drug users 
-  tmp2 <- subset(tmp, drug == 1)
-  tmp2 <- tmp2[complete.cases(tmp2),]
-  
-  # calculate the mean ome and plot it 
-  t0 <- mean(tmp2$totalopioiddose[which(tmp2$time == 0)], na.rm=T)
-  t1 <- mean(tmp2$totalopioiddose[which(tmp2$time == 1)], na.rm=T)
-  t2 <- mean(tmp2$totalopioiddose[which(tmp2$time == 2)], na.rm=T)
-  t3 <- mean(tmp2$totalopioiddose[which(tmp2$time == 3)], na.rm=T)
-  t4 <- mean(tmp2$totalopioiddose[which(tmp2$time == 4)], na.rm=T)
-  t5 <- mean(tmp2$totalopioiddose[which(tmp2$time == 5)], na.rm=T)
-  
-  time <- c(0:5)
-  means <- c(t0,t1,t2,t3,t4,t5)
-  
-  results <- data.frame(Time = time, OME = means)
-  return(results)
-}
 ##############################################################################
 #################################### END #####################################
 ##############################################################################
